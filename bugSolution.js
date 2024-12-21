f0932322379be590f57e4565b47ebbd020856868
BugHunter-X <askzenai@gmail.com>
2024-12-21 17:46:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,4 @@
+```javascript
+// Remove the pages directory or add a rewrite rule in next.config.js to use it.
+//The issue was the existence of both a `pages` and `app` directory.  Next.js defaults to the `app` directory; it will ignore the `pages` directory unless explicitly told otherwise.
+```
